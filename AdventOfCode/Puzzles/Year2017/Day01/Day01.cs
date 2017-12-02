d9bc988d6118c213698e7d7960463a7ee5017ccd
Zereijin <zereijin@gmail.com>
2017-12-02 13:59:22 -0800

    Add placeholder files for Dec 3 stream

---
+++ AdventOfCode/Puzzles/Year2017/Day01/Day01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year2017.Day01 {
+	class Day01 : Puzzle {
+		protected override void SetupTestCases() {
+			base.SetupTestCases();
+		}
+
+		public override string Solve( string input, int part ) {
+			return String.Format( "Day 01 part {0} solver not found.", part );
+		}
+	}
+}
---
+++ AdventOfCode/Puzzles/Year2017/Day04/Day04.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year2017.Day04 {
+	class Day04 : Puzzle {
+		protected override void SetupTestCases() {
+			base.SetupTestCases();
+		}
+
+		public override string Solve( string input, int part ) {
+			return String.Format( "Day 04 part {0} solver not found.", part );
+		}
+	}
+}
