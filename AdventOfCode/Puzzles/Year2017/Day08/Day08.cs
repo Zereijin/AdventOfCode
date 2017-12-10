e3c9b4adb62c4f3a7627645d32ddb8ad75dac58d
Zereijin <zereijin@gmail.com>
2017-12-10 00:36:30 -0800

    Prepare empty files for Dec 10 stream

---
+++ AdventOfCode/Puzzles/Year2017/Day08/Day08.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year2017.Day08 {
+	class Day08 : Puzzle {
+		protected override void SetupTestCases() {
+			base.SetupTestCases();
+		}
+
+		public override string Solve( string input, int part ) {
+			return String.Format( "Day 08 part {0} solver not found.", part );
+		}
+	}
+}
---
+++ AdventOfCode/Puzzles/Year2017/Day09/Day09.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year2017.Day09 {
+	class Day09 : Puzzle {
+		protected override void SetupTestCases() {
+			base.SetupTestCases();
+		}
+
+		public override string Solve( string input, int part ) {
+			return String.Format( "Day 09 part {0} solver not found.", part );
+		}
+	}
+}
