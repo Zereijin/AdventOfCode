294b6e2a6dd4ded99afd0f18d37041ad767d6bb5
Zereijin <zereijin@gmail.com>
2017-12-17 09:34:29 -0800

    Prepare for Dec 17 stream

---
+++ AdventOfCode/Puzzles/Year2017/Day14/Day14.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Puzzles.Year2017.Day14 {
+	class Day14 : Puzzle {
+		protected override void SetupTestCases() {
+			base.SetupTestCases();
+		}
+
+		public override string Solve( string input, int part ) {
+			return String.Format( "Day 14 part {0} solver not found.", part );
+		}
+	}
+}
