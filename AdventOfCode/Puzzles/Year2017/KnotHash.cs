41f29b9c24dfbbc42c41adeff9b45000563d68de
Zereijin <zereijin@gmail.com>
2017-12-20 19:42:20 -0800

    Refactor GetBin to remove magic strings

--- AdventOfCode/Puzzles/Year2017/KnotHash.cs
+++ AdventOfCode/Puzzles/Year2017/KnotHash.cs
@@ -186,25 +186,36 @@ namespace AdventOfCode.Puzzles.Year2017 {
 		}
 
 		private string GetBin( char hexDigit ) {
+			int remainder = hexDigit;
+			string result = "";
+
 			switch( hexDigit ) {
-				case '1': return "0001";
-				case '2': return "0010";
-				case '3': return "0011";
-				case '4': return "0100";
-				case '5': return "0101";
-				case '6': return "0110";
-				case '7': return "0111";
-				case '8': return "1000";
-				case '9': return "1001";
-				case 'a': return "1010";
-				case 'b': return "1011";
-				case 'c': return "1100";
-				case 'd': return "1101";
-				case 'e': return "1110";
-				case 'f': return "1111";
+				case 'a':
+					remainder = 10;
+					break;
+				case 'b':
+					remainder = 11;
+					break;
+				case 'c':
+					remainder = 12;
+					break;
+				case 'd':
+					remainder = 13;
+					break;
+				case 'e':
+					remainder = 14;
+					break;
+				case 'f':
+					remainder = 15;
+					break;
 			}
 
-			return "0000";
+			for( int i = 0; i < 4; i++ ) {
+				result = ( remainder % 2 ) + result;
+				remainder /= 2;
+			}
+
+			return result;
 		}
 	}
 }
